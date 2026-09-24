0cc772bcc0d27e5907f65aaed6dc56d1420879c2
Baptiste Rajaut <baptiste@rajaut.fr>
2026-09-24 12:15:55 +0200

    fix: render the prompt colours with %b

--- kubernetes2simple.sh
+++ kubernetes2simple.sh
@@ -375,7 +375,7 @@ render_helmfile() {
     elif [[ ! -f "$K2S_DIR/.ignoreNoHelmfileEnvironment" ]]; then
         warn "No --env specified. Helmfile projects often require an environment."
         warn "If this fails, re-run with: ./k2s.sh --env <environment>"
-        printf "%s[k2s]%s Press Enter to continue without an environment, or Ctrl+C to abort... " "$_yellow" "$_nc"
+        printf "%b[k2s]%b Press Enter to continue without an environment, or Ctrl+C to abort... " "$_yellow" "$_nc"
         if [[ -t 0 ]]; then
             read -r
         elif { : < /dev/tty; } 2>/dev/null; then
